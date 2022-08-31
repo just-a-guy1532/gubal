83764e7816cc8ca86dcd0b0a57afee94f6e565d8
Just a guy <103865615+just-a-guy1532@users.noreply.github.com>
2022-08-31 17:33:05 +0100

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -185,6 +185,11 @@ class FreeplayState extends MusicBeatState
 		text.setFormat(Paths.font("vcr.ttf"), size, FlxColor.WHITE, RIGHT);
 		text.scrollFactor.set();
 		add(text);
+
+                #if mobileC
+		addVirtualPad(FULL, A_B);
+		#end
+  
 		super.create();
 	}
 
